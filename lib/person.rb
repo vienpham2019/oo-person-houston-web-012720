6c194b477f35cce607f9653cd4bf98ce19cff7fe
vienpham2019 <vienpham2019@gmail.com>
2020-01-29 16:34:08 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -31,7 +31,8 @@ class Person
     end  
   
     def take_bath
-      self.hygiene += 4
+    #   self.hygiene += 4
+        hygiene(@hygiene += 4)
       '♪ Rub-a-dub just relaxing in the tub ♫'
     end 
   
